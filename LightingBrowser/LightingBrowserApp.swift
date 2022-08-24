29d9c925bfb9e1a73576ef55453e46e229e1ab8c
user1202 <yj@gmail.com>
2022-08-24 04:21:51 +0200

    no message

---
+++ LightingBrowser/LightingBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightingBrowserApp.swift
+//  LightingBrowser
+//
+//  Created by user1202 on 24/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct LightingBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
